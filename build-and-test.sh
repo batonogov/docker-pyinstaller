faebb1c68eaaa8a6692aaab530998fc6fcfa157c
Fedor Batonogov <f.batonogov@yandex.ru>
2023-02-27 20:14:02 +0300

    Update test

--- build-and-test.sh
+++ build-and-test.sh
@@ -4,6 +4,8 @@ if [ -z $1 ];
 then
     echo "Enter dockerfile name"
 else
-    docker build -f $1 -t test_image . && \
-    docker run -v "$(pwd)/test:/src/" test_image "pyinstaller main.py --onefile"
+    docker build -f $1 --platform=linux/arm64 -t test_arm64 . && \
+    docker build -f $1 --platform=linux/amd64 -t test_amd64 . && \
+    docker run -v "$(pwd)/test:/src/" test_arm64 "pyinstaller main.py --onefile" && \
+    docker run -v "$(pwd)/test:/src/" test_amd64 "pyinstaller main.py --onefile"
 fi
