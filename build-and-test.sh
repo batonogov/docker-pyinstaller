26808c831dc569d44b39bc4ccecc987d12315f3e
Fedor Batonogov <f.batonogov@yandex.ru>
2023-08-06 15:15:14 +0300

    Added pre commit hook (#24)
    * Added pre commit hook
    
    * Test pre commit

--- build-and-test.sh
+++ build-and-test.sh
@@ -5,5 +5,5 @@ then
     echo "Enter dockerfile name"
 else
     docker build -f $1 -t pyinstaller_test . && \
-    docker run -v "$(pwd)/test:/src/" pyinstaller_test "pyinstaller main.py --onefile"
+    docker run -v "$(pwd)/test:/src" pyinstaller_test "pyinstaller main.py --onefile"
 fi
