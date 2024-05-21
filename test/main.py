254c6d99f3e39a16eddea35878c3d11b1d7de459
Fedor Batonogov <f.batonogov@yandex.ru>
2024-05-21 06:52:23 +0300

    Update test

--- test/main.py
+++ test/main.py
@@ -1,4 +1,15 @@
 import requests
 
-get = requests.get("https://api.github.com")
-print(get)
+
+def check_requests():
+    response = requests.get("https://api.github.com")
+    print(f"Response Status Code: {response.status_code}")
+    print(f"Response Content: {response.json()}")
+
+
+def main():
+    check_requests()
+
+
+if __name__ == "__main__":
+    main()
